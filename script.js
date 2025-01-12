038f66c202494a4ae98bf935c9b820840e0698fa
Lucamagio <lucamagionami4@gmail.com>
2025-01-12 13:28:57 +0100

    feat: Aggiunta la validazione del Form sul file HTML e Js

--- script.js
+++ script.js
@@ -1,14 +1,74 @@
 const buttonPreventivo = document.querySelector('#buttonPreventivo')
-const codiceInput = document.querySelector('#codiceInput')
+
+//Funzione per la validazione dei campi del form
+function validazioneForm(){
+    isValid = true
+
+    //Validazione nome
+    const nameInput = document.querySelector('#nameInput')
+    if(nameInput.value.trim() === ''){
+        nameInput.classList.add('is-invalid')
+        isValid = false
+    } else{
+        nameInput.classList.remove('is-invalid')
+        nameInput.classList.add('is-valid')
+    }
+
+    //Validazione cognome
+    const surnameInput = document.querySelector('#surnameInput')
+    if(surnameInput.value.trim() === ''){
+        surnameInput.classList.add('is-invalid')
+        isValid = false
+    } else{
+        surnameInput.classList.remove('is-invalid')
+        surnameInput.classList.add('is-valid')
+    }
+
+    //Validazione email
+    const emailInput = document.querySelector('#emailInput')
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailRegex.test(emailInput.value)){
+        emailInput.classList.add('is-invalid')
+        isValid = false
+    } else{
+        emailInput.classList.remove('is-invalid')
+        emailInput.classList.add('is-valid')
+    }
+
+    //Validazione scelta lavoro
+    const selectWork = document.querySelector('#selectWork')
+    if(selectWork.value === ''){
+        selectWork.classList.add('is-invalid')
+        isValid = false
+    }else{
+        selectWork.classList.remove('is-invalid')
+        selectWork.classList.add('is-valid')
+    }
+
+    //Validazione Privacy Policy
+    const privacyCheckbox = document.querySelector('#flexCheckDefault');
+    if (!privacyCheckbox.checked) {
+        privacyCheckbox.classList.add('is-invalid');
+        isValid = false;
+    } else {
+        privacyCheckbox.classList.remove('is-invalid');
+        privacyCheckbox.classList.add('is-valid');
+    }
+
+    return isValid
+}
+
 
 //Funzione per il calcolo del preventivo
 function calcoloPreventivo(work){
-    
-    const scontoOttenuto = document.querySelector('#scontoOttenuto')
-    const coupon = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
-    const sconto = coupon.includes(codiceInput.value)
-    let costoOrario //variabile che va ad identificare il prezzo orario a seconda della scelta sul select
+
+    const codiceInput = document.querySelector('#codiceInput')
     const ore = 10 //numero delle ore date di default per il calcolo del preventivo
+    const coupons = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
+    const couponSconto = coupons.includes(codiceInput.value)
+
+    let costoOrario //variabile che va ad identificare il prezzo orario a seconda della scelta sul select
+    let scontoOttenuto //variabile di risposta al coupon inserito
     let preventivo
 
     if (work === '1'){
@@ -19,13 +79,19 @@ function calcoloPreventivo(work){
         costoOrario = 33.6
     }
 
-    if(sconto){
-        preventivo = (costoOrario * ore) * 0.75
-        scontoOttenuto.classList.remove('d-none')
-    } else{
-        preventivo = (costoOrario * ore)
-        scontoOttenuto.classList.add('d-none')
+    if(couponSconto){
+        preventivo = ((costoOrario * ore) * 0.75).toFixed(2)
+        scontoOttenuto = `<p class="alert alert-success text-center">Hai ottenuto uno sconto del 25%</p>`
+    } else if(couponSconto != codiceInput.value){
+        preventivo = (costoOrario * ore).toFixed(2)
+        scontoOttenuto = `<p class="alert alert-danger text-center">Coupon inserito non valido o già riscattato</p>`
+    } else if(!codiceInput.value) {
+        preventivo = (costoOrario * ore).toFixed(2)
+        scontoOttenuto = ''
     }
+    document.getElementById('scontoOttenuto').innerHTML = scontoOttenuto
+
+    console.log(preventivo)
     
     return preventivo
 }
@@ -34,10 +100,19 @@ function calcoloPreventivo(work){
 buttonPreventivo.addEventListener('click', function(event){
     event.preventDefault()
 
+    //Validazione del Form
+    if(!validazioneForm()){
+
+        //Blocca la validazione è negativa
+        return
+    }
+
     const selectWork = document.querySelector('#selectWork').value
     const result = document.querySelector('#result')
 
     result.innerHTML = `€ ${calcoloPreventivo(selectWork)}`
+
+    
 
 })
 
