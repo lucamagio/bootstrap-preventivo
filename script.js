4253f333d7ef20323cb608eb38baf1875aa33716
Lucamagio <lucamagionami4@gmail.com>
2025-01-15 19:51:04 +0100

    Pulizia codice e fix della card

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
+const backendDevelopment = 'backendDevelopment'
+const frontendDevelopment = 'frontendDevelopment'
+const projectAnalysis = 'projectAnalysis'
+
 //Funzione per la validazione dei campi del form
 function validazioneCampo(input, condizione){
     if (condizione) {
         input.classList.remove('is-invalid');
         input.classList.add('is-valid');
-    } else {
-        input.classList.add('is-invalid');
-        input.classList.remove('is-valid');
-        return false;
+        return true
+
     }
 
-    return true
-   }
+    input.classList.add('is-invalid');
+    input.classList.remove('is-valid');
+    return false;
+}
 
+function calcolaCostoOrario(work){
+
+    //Selezione della tipologia di lavoro
+    switch(work){
+        case backendDevelopment:
+            return 20.5
+        case frontendDevelopment:
+            return 15.3
+        case projectAnalysis:
+            return 33.6
+    }
+}
 
 //Funzione per il calcolo del preventivo
 function calcoloPreventivo(work){
@@ -21,17 +37,9 @@ function calcoloPreventivo(work){
     const coupons = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
     const couponSconto = coupons.includes(couponInput.value)
 
-    let costoOrario //variabile che va ad identificare il prezzo orario a seconda della scelta sul select
+    const costoOrario = calcolaCostoOrario(work) //variabile che va ad identificare il prezzo orario a seconda della scelta sul select
     let scontoOttenuto
 
-    //Selezione della tipologia di lavoro
-    if (work === '1'){
-        costoOrario = 20.5
-    } else if(work === '2'){
-        costoOrario = 15.3
-    } else if(work === '3'){
-        costoOrario = 33.6
-    }
 
     //Calcolo preventivo con o senza sconto
     let preventivo = costoOrario * ore
@@ -39,12 +47,14 @@ function calcoloPreventivo(work){
     if(!couponInput.value) {
         preventivo = preventivo.toFixed(2)
         scontoOttenuto = ''
-    } else if(couponSconto){
-        preventivo = (preventivo * 0.75).toFixed(2)
-        scontoOttenuto = `<p class="alert alert-success text-center">Hai ottenuto uno sconto del 25%</p>`
     } else{
-        preventivo = preventivo.toFixed(2)
-        scontoOttenuto = `<p class="alert alert-danger text-center">Coupon inserito non valido o già riscattato</p>`
+        if(couponSconto){
+            preventivo = (preventivo * 0.75).toFixed(2)
+            scontoOttenuto = `<p class="alert alert-success text-center">Hai ottenuto uno sconto del 25%</p>`
+        } else{
+            preventivo = preventivo.toFixed(2)
+            scontoOttenuto = `<p class="alert alert-danger text-center">Coupon inserito non valido o già riscattato</p>`
+        }
     }
 
     //Stampa a video se lo sconto è avvenuto oppure no
@@ -53,42 +63,42 @@ function calcoloPreventivo(work){
     return preventivo.replace('.', ',')
 }
 
-const buttonPreventivo = document.querySelector('#buttonPreventivo')
+const form = document.querySelector('#validationForm')
 
 //Evento click del bottono 'calcolo preventivo'
-buttonPreventivo.addEventListener('click', function(event){
+form.addEventListener('submit', function(event){
     event.preventDefault()
 
     let isValid = true
 
     //Validazione nome
     const nameInput = document.querySelector('#nameInput')
-    isValid &= validazioneCampo(nameInput, nameInput.value.trim() !== '')
+    isValid = validazioneCampo(nameInput, nameInput.value.trim() !== '') && isValid
 
     //Validazione cognome
     const surnameInput = document.querySelector('#surnameInput')
-    isValid &= validazioneCampo(surnameInput, surnameInput.value.trim() !== '')
+    isValid =  validazioneCampo(surnameInput, surnameInput.value.trim() !== '') && isValid
 
     //Validazione email
     const emailInput = document.querySelector('#emailInput')
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    isValid &= validazioneCampo(emailInput, emailRegex.test(emailInput.value))
+    isValid = validazioneCampo(emailInput, emailRegex.test(emailInput.value)) && isValid
 
     //Validazione scelta lavoro
     const selectWork = document.querySelector('#selectWork')
-    isValid &= validazioneCampo(selectWork, selectWork.value !== '')
+    isValid = validazioneCampo(selectWork, selectWork.value !== '') && isValid
 
     //Validazione informazioni aggiuntive
     const textareaInput = document.querySelector('#textareaInput')
-    isValid &= validazioneCampo(textareaInput, textareaInput.value !== '')
+    isValid = validazioneCampo(textareaInput, textareaInput.value !== '') && isValid
 
     //Validazione Privacy Policy
     const privacyCheckbox = document.querySelector('#flexCheckDefault');
-    isValid &= validazioneCampo(privacyCheckbox, privacyCheckbox.checked)
+    isValid = validazioneCampo(privacyCheckbox, privacyCheckbox.checked) && isValid
 
     //In caso di validazione confermata: Calcolo del preventivo
     const result = document.querySelector('#result')
-    campiError = document.querySelector('#campiError')
+    const campiError = document.querySelector('#campiError')
 
     //Guarda il risultato della validazione del form
     if(!isValid){
